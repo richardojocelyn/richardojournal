e71ba5b6cf5093e147d552596e9057e9ebefcc2b
alaric d <lerirotasi@gmail.com>
2026-04-19 12:00:33 +0700

    Paksa add notification file

--- api/notification.js
+++ api/notification.js
@@ -1,8 +1,3 @@
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-        console.log("Notifikasi masuk dari Midtrans:", req.body);
-        return res.status(200).json({ status: 'OK' });
-    }
-    // Jika dibuka lewat browser (GET)
-    return res.status(200).send("Pintu sudah aktif, tinggal nunggu kiriman dari Midtrans!");
+  res.status(200).json({ message: "Pintu kebuka, Cad!" });
 }
