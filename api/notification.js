37e7a667a295488244e26b0e1742ed13ea19bea9
alaric d <lerirotasi@gmail.com>
2026-04-19 12:14:28 +0700

    Sapu bersih Vercel config & update notif

--- api/notification.js
+++ api/notification.js
@@ -1,3 +1,15 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Pintu kebuka, Cad!" });
+export default async function handler(req, res) {
+    // 1. Kalau lu ngetes buka link ini di browser (Jalur GET)
+    if (req.method === 'GET') {
+        return res.status(200).send("GACOR CAD! PINTU UDAH KEBUKA DAN SIAP NERIMA MIDTRANS!");
+    }
+
+    // 2. Kalau Midtrans ngirim laporan pembayaran (Jalur POST)
+    if (req.method === 'POST') {
+        console.log("🔥 ADA YANG BAYAR NIH CAD! Data:", req.body);
+        return res.status(200).json({ status: 'OK', message: 'Notif aman diterima' });
+    }
+
+    // Kalau ada yang nyoba-nyoba jalur lain
+    return res.status(405).json({ message: "Jalur kaga diizinin" });
 }
